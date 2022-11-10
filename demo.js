445d81c62b63398db8f5cf0f0ba0ea876eba2d7d
tienlam <lamtien98@gmail.com>
2022-11-11 02:05:20 +0700

    e10.2

--- demo.js
+++ demo.js
@@ -38,6 +38,8 @@ function taoPoint2D() {
 }
 taoPoint2D();
 
+
+
 //bai 2 Circle
 class Circle {
     constructor(tam, banKinh) {
@@ -49,32 +51,87 @@ class Circle {
         const y = Number(readline.question('nhap tung do point2D moi '));
         const newTam = new Point2D(x, y);
         this.tam = newTam;
+    }
+    print() {
         console.log(this);
+    }
+    getArea() {
+        const dienTich = this.banKinh ** 2 * 3.14;
+        console.log(dienTich);
     }
 }
 
 function chayBai2() {
-    const n2 = new Point2D(4, 5);
+    const n2 = new Point2D(4, 4);
+    const n3 = new Point2D(5, 5);
     const circle1 = new Circle(n2, 5);
+    const circle2 = new Circle(n3, 6);
     console.log(circle1);
     circle1.scan();
+    circle1.print();
+    circle1.getArea();
+    circle2.getArea();
 }
 chayBai2();
 
 class Rectangle {
     constructor(toaDo, dai, rong) {
-        this.toaDo = Point2D;
-        this.dai = x;
-        this.rong = y;
+        this.toaDo = toaDo;
+        this.dai = dai;
+        this.rong = rong;
     }
     scan() {
-
+        const x = Number(readline.question('nhap hoanh do point2D moi '));
+        const y = Number(readline.question('nhap tung do point2D moi '));
+        const toaDoMoi = new Point2D(x, y);
+        this.toaDo = toaDoMoi;
+    }
+    print() {
+        console.log(this.toaDo);
+    }
+    getArea() {
+        const dienTichChuNhat = this.dai * this.rong;
+        console.log(`dien tich hinh chu nhat la ${dienTichChuNhat}`);
     }
 }
 
 function chayBai03() {
-    // let Point2D = taoPoint2D();
-    console.log(toaDo);
-
+    const r1 = new Point2D(5, 10);
+    const rectangle1 = new Rectangle(r1, 20, 10);
+    console.log(rectangle1);
+    rectangle1.print();
+    rectangle1.getArea();
 }
 chayBai03();
+
+
+// bai03 Square
+class Square {
+    constructor(toaDoHinhVuong, chieuDaiCanh) {
+        this.toaDoHinhVuong = toaDoHinhVuong;
+        this.chieuDaiCanh = chieuDaiCanh;
+    }
+    scan() {
+        const x = Number(readline.question('nhap hoanh do moi cho hinh vuong: '));
+        const y = Number(readline.question('nhap tung do moi cho hinh vuong: '));
+        const toaDoHinhVuongMoi = new Point2D(x, y);
+        this.toaDoHinhVuong = toaDoHinhVuongMoi;
+    }
+    print() {
+        console.log(this.toaDoHinhVuong);
+    }
+    getArea() {
+        const dienTichHinhVuong = this.chieuDaiCanh **2;
+        console.log(`dien tich hinh vuong la: ${dienTichHinhVuong}`);
+    }
+}
+
+function chayBai04() {
+    const a1 = new Point2D(8, 8);
+    const square1 = new Square(a1, 10);
+    console.log(square1);
+    square1.scan();
+    square1.print();
+    square1.getArea();
+}
+chayBai04();
